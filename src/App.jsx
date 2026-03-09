e31a02c19ce382232bc2be00da51d2e6be72aecf
hakkinenjoonas-ctrl <hakkinen.joonas@gmail.com>
2026-03-09 10:17:07 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>Suoraan Kalastajalta</h1>
+      <p>App toimii.</p>
+    </div>
+  );
+}
